bafe56a6863672c681c3cf671f5e10b20abf2eaa
Andy B <andybarnov@gmail.com>
2021-01-11 19:17:05 +0100

    quickfix

--- entrypoint.rb
+++ entrypoint.rb
@@ -20,7 +20,7 @@ def query_check_status(ref, check_name, token, workflow_name)
   }
   parsed = JSON.parse(response.body)
 
-  parsed["check_runs"].reject { |check| check['name'] == workflow_name }
+  parsed["check_runs"].reject { |check| check["name"] == workflow_name }
 end
 
 def all_checks_complete(checks)
@@ -39,7 +39,7 @@ if !check_name.empty? && all_checks.empty?
 end
 
 until all_checks_complete(all_checks)
-  plural_part = all_other_checks.length > 1 ? "checks aren't" : "check isn't"
+  plural_part = all_checks.length > 1 ? "checks aren't" : "check isn't"
   puts "The requested #{plural_part} complete yet, will check back in #{wait} seconds..."
   sleep(wait)
   all_checks = query_check_status(ref, check_name, token, workflow_name)
