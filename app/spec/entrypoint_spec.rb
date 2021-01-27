5b7f7f01ac596c49721066e6d22b888d39c23f8f
Billy Mathews <billy@billym.co.uk>
2021-01-27 13:38:09 +0000

    test: fix entrypoint test

--- app/spec/entrypoint_spec.rb
+++ app/spec/entrypoint_spec.rb
@@ -1,8 +1,11 @@
 require "spec_helper"
 
+
 describe "entrypoint" do
   it "calls the GithubChecksVerifier service" do
     allow(GithubChecksVerifier).to receive(:call)
+    allow(GithubChecksVerifier).to receive(:configure)
+
     require_relative "../../entrypoint.rb"
 
     expect(GithubChecksVerifier).to have_received(:call)
