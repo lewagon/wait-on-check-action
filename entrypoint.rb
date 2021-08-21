d2bf513140d88c5adc9d385f22863b1cec4f9b18
Matias Albarello <m.albarello@buddyandselly.com>
2021-08-21 22:12:13 +0200

    Order input args alphabetically

--- entrypoint.rb
+++ entrypoint.rb
@@ -2,7 +2,7 @@
 require_relative "./app/services/github_checks_verifier.rb"
 require "octokit"
 
-allowed_conclusions, ref, check_name, check_regexp, token, verbose, wait, workflow_name  = ARGV
+allowed_conclusions, check_name, check_regexp, ref, token, verbose, wait, workflow_name  = ARGV
 
 GithubChecksVerifier.configure do |config|
   config.allowed_conclusions = allowed_conclusions.split(',').map(&:strip)
